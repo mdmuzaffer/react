41300ad7d13816846bc3072daf219fd9cb2314e7
mdmuzaffer <skkhan9708@gmail.com>
2021-12-26 23:30:15 +0530

    Updates

---
+++ precache-manifest.d3b94fc67f2d4a7295ab50f894a46f3e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f4874b877f7ee56c458",
+    "url": "/react/static/css/main.da95a50f.chunk.css"
+  },
+  {
+    "revision": "5f4874b877f7ee56c458",
+    "url": "/react/static/js/main.5f4874b8.chunk.js"
+  },
+  {
+    "revision": "aeb7b420299a970318c2",
+    "url": "/react/static/js/1.aeb7b420.chunk.js"
+  },
+  {
+    "revision": "6a9f0a62b32257b29cb9",
+    "url": "/react/static/css/2.7c6f6666.chunk.css"
+  },
+  {
+    "revision": "6a9f0a62b32257b29cb9",
+    "url": "/react/static/js/2.6a9f0a62.chunk.js"
+  },
+  {
+    "revision": "585c1dde0b089d501758",
+    "url": "/react/static/js/runtime~main.585c1dde.js"
+  },
+  {
+    "revision": "e62172444c8537d06c3c2f3ed20001f5",
+    "url": "/react/static/media/slider3.e6217244.jpg"
+  },
+  {
+    "revision": "105f89ee44573670b03ae3514ed55b81",
+    "url": "/react/static/media/digital.105f89ee.jpg"
+  },
+  {
+    "revision": "ee1232552157d9c2ef570ef5d7fc38b9",
+    "url": "/react/static/media/slider1.ee123255.jpg"
+  },
+  {
+    "revision": "ecbf03c91ceedf8335a4f1a3cdd071bd",
+    "url": "/react/static/media/slider2.ecbf03c9.jpg"
+  },
+  {
+    "revision": "d3d78896c8f0b3a03b3f0d04085f9edc",
+    "url": "/react/static/media/service0.d3d78896.jpg"
+  },
+  {
+    "revision": "588c61262a61995561a964e8ca5a0729",
+    "url": "/react/static/media/service2.588c6126.jpg"
+  },
+  {
+    "revision": "72fe8a94b5ee7abe54268a2d52799c55",
+    "url": "/react/static/media/service3.72fe8a94.jpg"
+  },
+  {
+    "revision": "ee36751ae91ebb5e78589e937bf223e7",
+    "url": "/react/static/media/service4.ee36751a.jpg"
+  },
+  {
+    "revision": "48e7d367ef74c3157d0a265f59ec6609",
+    "url": "/react/static/media/service5.48e7d367.jpg"
+  },
+  {
+    "revision": "7c015a56fbdbcba6b5f32675e61fa07c",
+    "url": "/react/static/media/service6.7c015a56.jpg"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "9597a6246236f1bd39b7db8082d3e13b",
+    "url": "/react/static/media/service1.9597a624.jpg"
+  },
+  {
+    "revision": "c9d5a01ac344243cbc1109ca4728184d",
+    "url": "/react/index.html"
+  }
+];
